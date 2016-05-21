2c667a0d6c35ee855dd45ab812705fa155a1b326
we <wei@163.com>
2016-05-21 13:58:43 +0800

    验证

--- Application/Console/Model/StaffModel.class.php
+++ Application/Console/Model/StaffModel.class.php
@@ -4,22 +4,22 @@ use Think\Model;
 class StaffModel extends Model {
     // 定义自动验证
     protected $_validate    =   array(
-        //array(验证字段1,验证规则,错误提示,[验证条件,附加规则,验证时间]),
-        array('protype','require','类别必须填写'),
-        array('product','require','产品名称必须填写'),
-        array('price1','require','成本价格必须填写'),
-        array('price2','require','标准价格必须填写'),
-        array('price3','require','最高价格必须填写'),
-        array('discount','require','最低折扣必须填写'),
-        array('inputnum','require','进货数量必须填写'),
-        array('sortnum','require','剩余库存必须填写'),
-        array('salenum','require','销售数量必须填写'),
-        array('purchaseper','require','采购负责人必须填写'),
-        array('product','','设备编号已经存在！',2,'unique',1),// 在新增的时候验证name字段是否唯一
+        //array(验证字段1,验证规则,错误提示,[验证条件,附加规则,验证时间]),           
+        array('username','require','缺少用户名'),
+        array('username','','用户已存在',self::MUST_VALIDATE,'unique',self::MODEL_INSERT),
+        array('password','require','缺少密码'),
+        array('password','5,12','密码必须长度是5-12位',self::MUST_VALIDATE,'length'),
+        array('nickname','','改昵称已被使用过',self::MUST_VALIDATE,'unique',self::MODEL_INSERT),
+        array('name','require','姓名必须填写'),     
+        array('identity_card','15,18','请填写有效的身份证号码',self::MUST_VALIDATE,'length'),
+        array('mobile','require','联系方式必须填写'),
+        array('bank_account','16','银行卡号不正确',self::MUST_VALIDATE,'length'),
         );
     // 定义自动完成
     protected $_auto = array( 
          array('addtime','time',1,'function'), // 对update_time字段在更新的时候写入当前时间戳
+         array('create_time','time',self::MODEL_BOTH,'function'),        
+         array('update_time','time',self::MODEL_BOTH,'function'),        
      );
 
  }
