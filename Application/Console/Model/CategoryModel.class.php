1ffebf74a36d0523dbe4e6380dcce98eac25a079
zharty <zharty@163.com>
2016-05-21 17:17:53 +0800

    小改动

--- Application/Console/Model/CategoryModel.class.php
+++ Application/Console/Model/CategoryModel.class.php
@@ -4,11 +4,11 @@ use Think\Model;
 class CategoryModel extends Model {
     protected $tablename="category";
 
-    public function department(){
-         if(!I('get.id')){
+    public function department($map=array()){
+         if(!I('get.catid')){
             $map['cate_parent']=0;
         }else{
-            $map['cate_parent']=I('get.id');
+            $map['cate_parent']=I('get.catid');
         }        
         $map['is_show']=1;
         $data=D('Category')->where($map)->select();                    
