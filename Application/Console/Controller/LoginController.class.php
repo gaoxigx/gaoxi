c899bd8d06ba8ebe65945fab493692f53adb6d06
zharty <zharty@163.com>
2016-05-27 10:44:28 +0800

    数据

--- Application/Console/Controller/LoginController.class.php
+++ Application/Console/Controller/LoginController.class.php
@@ -37,45 +37,43 @@ class LoginController extends Controller {
         
     public function dologin()
     {
-        if (IS_POST) { 
+        if (!IS_POST) { 
+            $this->error('请重新提交数据');
+            exit();
+        }
             
-            //校对验证码
-            $verify = new \Think\Verify();    
-            if(!$verify->check($_POST['cpatcha'])){ 
-                $this->error('验证码错误');
-                exit;  
-            }
-            
-        if(!empty($_POST)){
-        $model = D('controller');//实例化一个(对象),每个对象对应着一张数据表
-        $username = I('post.username'); 
-        $password = I('post.password');
+        //校对验证码
+        $verify = new \Think\Verify();    
+        if(!$verify->check($_POST['cpatcha'])){ 
+            $this->error('验证码错误');
+            exit;  
+        }
         
-        //验证用户名密码 (没有数据的时候:select返回空数组,find返回null);
-        $admin_list = $model->field('username,id')->where("accounts = '$username' and  password = '".md5($password)."'")->find();    
-
-        if (!empty($admin_list)) {
+        if(!empty($_POST)){
+            $model = D('controller');//实例化一个(对象),每个对象对应着一张数据表
+            $username = I('post.username'); 
+            $password = I('post.password');
             
-            session ( 'userid', $admin_list ['id'] );
-            session ( 'username', $username );
-            session ( 'roleid', $admin_list ['roleid'] );
-             
-            //记住密码
-            if (isset($_POST['remember'])) {
-                $remeber = json_encode([
-                    'username' => $username,
-                    'password' => $this->authcode($_POST['password'], 'ENCODE'),
-                ]);
-                cookie('remember', $remeber);
-            }
-                      
-            //写入session
-            session('?username', $admin_list);
-            
-            //$_SESSION['admin'] = $admin;
+            //验证用户名密码 (没有数据的时候:select返回空数组,find返回null);
+            $admin_list = $model->field('username,id')->where("accounts = '$username' and  password = '".md5($password)."'")->find();    
 
-            //更新登录信息
-
+            if (!empty($admin_list)) {            
+                session ( 'userid', $admin_list ['id'] );
+                session ( 'username', $username );
+                session ( 'roleid', $admin_list ['roleid'] );
+                 
+                //记住密码
+                if (isset($_POST['remember'])) {
+                    $remeber = json_encode([
+                        'username' => $username,
+                        'password' => $this->authcode($_POST['password'], 'ENCODE'),
+                    ]);
+                    cookie('remember', $remeber);
+                }                      
+                //写入session
+                session('username', $admin_list);            
+      
+                //更新登录信息
                 $update = [
                    'id'=>$admin['id'],
                    'last_login_time'=>time(),
@@ -84,18 +82,16 @@ class LoginController extends Controller {
                 $model->save($update);          
 
                 $this->success('登录成功',  U('/Console/Index/main'));
-                exit();
-              
-                } else {
-      
-                    $this->error('用户名或密码不正确！');
-                    exit();
-                }
-            } else {
+                exit();                  
+            } 
+
+
+            $this->error('用户名或密码不正确！');
+            exit();
+            
+        } else {
                 //显示模板
-
                 $data = [];
-
                 if ($r = cookie('remember')) {
                     $r = json_decode($r, true);
                     $rememberUsername = $r['username'];
@@ -104,14 +100,10 @@ class LoginController extends Controller {
                     $data['username'] = $rememberUsername;
                     $data['password'] = $rememberPassword;
                 }
-
-                layout(false);
-    //           C('LAYOUT_ON', false);
-
                 $this->assign($data);
                 $this->display();
-            }
-        } 
+        }
+         
     }       
         
         
