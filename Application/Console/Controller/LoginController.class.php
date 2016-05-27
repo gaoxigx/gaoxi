e6c583a4fe9533e301d3908f1a1fc514d6a85d61
we <wei@163.com>
2016-05-27 14:30:39 +0800

    登陆

--- Application/Console/Controller/LoginController.class.php
+++ Application/Console/Controller/LoginController.class.php
@@ -5,7 +5,7 @@ namespace Console\Controller;
 use Think\Controller;
 header("Content-Type: text/html; charset=utf-8");
 class LoginController extends Controller {
-	
+    
     // 登陆页面
     public function index() {
         $this->display ();
@@ -17,18 +17,20 @@ class LoginController extends Controller {
         $Verify = new \Think\Verify();
         $Verify->imageW = 140;
         $Verify->imageH = 38;
-        $Verify->fontSize = 18;
-        $Verify->useNoise = tuer;
+        $Verify->fontSize = 20;
+        $Verify->useCurve = false;
+        $Verify->useNoise = true;
+        $Verify->length = 4;
         $Verify->entry();        
     }
 
         
-	// 前端验证码较验
-//	public function chk_code($code) {
-//		$verify = new \Think\Verify ();
-//		$res = $verify->check ( $code );
-//		echo $res;
-//	}
+    // 前端验证码较验
+//  public function chk_code($code) {
+//      $verify = new \Think\Verify ();
+//      $res = $verify->check ( $code );
+//      echo $res;
+//  }
 //        
         
 
@@ -37,53 +39,8 @@ class LoginController extends Controller {
         
     public function dologin()
     {
-        if (!IS_POST) { 
-            $this->error('请重新提交数据');
-            exit();
-        }
-            
-        //校对验证码
-        $verify = new \Think\Verify();    
-        if(!$verify->check($_POST['cpatcha'])){ 
-            $this->error('验证码错误');
-            exit;  
-        }
-        
-        if(!empty($_POST)){
-            $model = D('controller');//实例化一个(对象),每个对象对应着一张数据表
-            $StaffUser = D('staff');
-            $username = I('post.username'); 
-            $password = I('post.password');
-            
-            //验证用户名密码 (没有数据的时候:select返回空数组,find返回null);
-            $admin_list = $model->field('username,id')->where("accounts = '$username' and  password = '".md5($password)."'")->find();    
-
-            if (!empty($admin_list)) {    
-                //写入session
-                session ( 'userid', $admin_list ['id'] );
-                session ( 'username', $username );
-                session ( 'roleid', $admin_list ['roleid'] );
-               // session('username', $admin_list); 
-                $this->success('登录成功',  U('/Console/Index/main'));
-                exit(); 
-            } else {
-                
-                    $staff_list = $StaffUser->field('username,id')->where("username = '$username' and  password = '".md5($password)."'")->find();    
-
-                    if (!empty($staff_list)){            
-                        session ( 'username', $username );
-                        session ( 'section', $staff_list ['section'] );  
-                        //session('username', $staff_list);  
-                        $this->success('登录成功',  U('/Console/Index/main'));
-                        exit();  
-                    } else {
-                        $this->error('用户名或密码不正确！');
-                        exit();
-                    }
-                }
-            } else {
-                //显示模板
-                $data = [];
+        if (!IS_POST) {             
+              //显示模板           
                 if ($r = cookie('remember')) {
                     $r = json_decode($r, true);
                     $rememberUsername = $r['username'];
@@ -93,8 +50,44 @@ class LoginController extends Controller {
                     $data['password'] = $rememberPassword;
                 }
                 $this->assign($data);
-                $this->display();
-            }
+                $this->display();       
+                exit();
+        }
+            
+        //校对验证码
+        $verify = new \Think\Verify();    
+        if(!$verify->check($_POST['cpatcha'])){ 
+            $this->error('验证码错误');
+            exit;  
+        }
+        
+  
+            $model = D('controller');//实例化一个(对象),每个对象对应着一张数据表
+            $StaffUser = D('staff');
+            $username = I('post.username'); 
+            $password = I('post.password');
+            
+            //验证用户名密码 (没有数据的时候:select返回空数组,find返回null);
+            $admin_list = $model->field('username,id')->where("accounts = '$username' and  password = '".md5($password)."'")->find();  
+            if (!empty($admin_list)) {    
+                //写入session
+                session ( 'userid', $admin_list ['id'] );
+                session ( 'username', $username );
+                session ( 'roleid', $admin_list ['roleid'] );
+               // session('username', $admin_list); 
+                $this->success('登录成功',  U('/Console/Index/main'));
+                exit(); 
+            }                
+            $staff_list = $StaffUser->field('username,id')->where("username = '$username' and  password = '".md5($password)."'")->find(); 
+            if (!empty($staff_list)){            
+                session ( 'username', $username );
+                session ( 'section', $staff_list ['section'] );  
+                //session('username', $staff_list);  
+                $this->success('登录成功',  U('/Console/Index/main'));
+                exit();  
+            }         
+            
+            $this->error('用户名或密码不正确！');
     }       
           
         
