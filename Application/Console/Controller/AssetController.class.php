bd87048f9b428497dbe145d59ad43af76f1d2b97
zharty <zharty@163.com>
2016-06-12 13:13:43 +0800

    增加

--- Application/Console/Controller/AssetController.class.php
+++ Application/Console/Controller/AssetController.class.php
@@ -9,26 +9,30 @@ class AssetController extends CommonController{
 	}
         
  //添加页面       
-        public function add(){
-            $jumpUrl = U('Console/Asset/index');
-            $asset = D('Asset');            
-            if(!IS_POST){
-                 $this->display(); 
-                 exit();
+    public function add(){
+        $jumpUrl = U('Console/Asset/index');
+        $asset = D('Asset');            
+        if(!IS_POST){
+             $this->display(); 
+             exit();
+        }
+        unset($_POST['name']);
+        $data = $asset->create();    
+        if($data){    
+            $result = $asset->add($data);
+            if($result){
+                $this->success('添加成功!',$jumpUrl);
+            }else{
+                $this->error('添加失败!');
             }
-            unset($_POST['name']);
-            $data = $asset->create();    
-            if($data){    
-                $result = $asset->add($data);
-                if($result){
-                    $this->success('添加成功!',$jumpUrl);
-                }else{
-                    $this->error('添加失败!');
-                }
-            } else {
-              $this->error($asset->getError());
-            }            
-        }    
+        } else {
+          $this->error($asset->getError());
+        }            
+    } 
+    public function  staffAsset(){
+    	$this->display();
+    	
+    }
         
 }
 ?>
