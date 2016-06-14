c80d49bcbea4ee4302de0ab4419b87b65e4d0a99
zharty <zharty@163.com>
2016-06-14 13:39:58 +0800

    资产

--- Application/Console/Model/AssetModel.class.php
+++ Application/Console/Model/AssetModel.class.php
@@ -6,10 +6,8 @@ class AssetModel extends Model{
     protected $_validate    =   array(
       //  array(验证字段1,验证规则,错误提示,[验证条件,附加规则,验证时间]),           
         array('name','require','缺少用户名'),
-        array('name','','用户已存在',2,'unique',3),
-        array('usage','require','须填写用途'),
-        array('money','require','请输入金额'),
-        array('type','require','须填写类型'),
+        array('name','','用户已存在',2,'unique',3),        
+        array('money','require','请输入金额'),        
         );
     
     // 定义自动完成
