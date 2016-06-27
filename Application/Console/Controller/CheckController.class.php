858e2260059d89e72808bd99e562129ee01d2390
liu <1227642768@qq.com>
2016-06-27 14:12:35 +0800

    考勤修改

--- Application/Console/Controller/CheckController.class.php
+++ Application/Console/Controller/CheckController.class.php
@@ -20,13 +20,27 @@ class CheckController extends CommonController{
     
 //列表    
 public function index(){
-    $list = D('check')->where($map)->select();
+	$keywords = I('keywords');
+	if($keywords != ''){
+		$map['name'] = array('like','%'.$keywords.'%');
+		$map['bumen'] = array('like','%'.$keywords.'%');
+		$map['_logic'] = 'or';
+		$parameter['keywords'] = $keywords;
+	}
+	$count = D('check')->where($map)->count();// 查询满足要求的总记录数
+      
+	$Page = new \Think\Page($count,17,$parameter);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+    $show = $Page->show();// 分页显示输出
+	
+	$list = D('check')->where($map)->limit($Page->firstRow.','.$Page->listRows)->select();
     foreach($list as $k=>$v){
         $data['calculated'] = $v['calculated'];
     }
-    
+    print_r(M()->getLastsql());
     $this->assign('data',$data);
     $this->assign('list',$list);
+	$this->assign('parameter',$parameter);
+	$this->assign('page',$show);
     $this->display();
 }  
     
--- Application/Console/Controller/CheckRecordController.class.php
+++ Application/Console/Controller/CheckRecordController.class.php
@@ -30,7 +30,7 @@ class CheckRecordController extends CommonController{
 		$count = $CheckRecord->where($map)->count();// 查询满足要求的总记录数
         
 		
-		$Page = new \Think\Page($count,18,$parameter);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$Page = new \Think\Page($count,17,$parameter);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show = $Page->show();// 分页显示输出
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
 		$list = $CheckRecord->where($map)->order('gh_id asc,datetime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
