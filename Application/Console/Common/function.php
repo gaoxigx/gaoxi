c9ef057eeeda307f0e62feeaff479b65bbdf23b8
zharty <zharty@163.com>
2016-09-01 11:54:14 +0800

    隐藏

--- Application/Console/Common/function.php
+++ Application/Console/Common/function.php
@@ -89,10 +89,11 @@ function json_ary($json){
     //隐藏字段方法
     function hiddenstr($str){
         if(strlen($str)>6&&strlen($str)<10){      
-            return substr_replace($str,'**',3,strlen($str)-6);
+            return substr_replace($str,'**',4,strlen($str)-6);
         }
-        if(strlen($str)<=6){
-            return substr_replace($str,'**',0,strlen($str)-3);
+
+        if(strlen($str)<=9){
+            return substr_replace($str,'**',0,strlen($str)-4);
         }
         return substr_replace($str,'***',3,strlen($str)-6);
 
