fa1dc8e9cd6516b841645e512eb924fc65ac5bf1
zharty <zharty@163.com>
2016-09-03 10:56:42 +0800

    bug

--- Application/Console/Controller/ProductController.class.php
+++ Application/Console/Controller/ProductController.class.php
@@ -76,7 +76,7 @@ class ProductController extends CommonController {
         $User = M('Product'); // 实例化User对象
         $count = $User->where($map)->count();// 查询满足要求的总记录数
 		
-        $Page = new \Think\Page($count,6,$parameter);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $Page = new \Think\Page($count,15,$parameter);// 实例化分页类 传入总记录数和每页显示的记录数(25)
 		$show = $Page->show();// 分页显示输出
        
         $list = $User->where($map)->order('id desc')->limit($Page->firstRow.','.$Page->listRows)->order('salenum desc')->select();
