c5eb853ae28cc33d1a0b1f3f087c305667d12b07
zharty <zharty@163.com>
2017-07-05 17:30:32 +0800

    数据

--- Application/Console/Controller/GoodsController.class.php
+++ Application/Console/Controller/GoodsController.class.php
@@ -54,9 +54,16 @@ class GoodsController extends Controller {
    			$where['cd.userid']=session("userid");
    		}
     	
-    	$list=D("coding")->field("cd.*,gs.name")->alias("cd")->join("__GOODS__ gs on gs.coding=cd.code",'left')->order("cd.createtime","desc")->where($where)->select();
+    	$coding = M('coding'); // 实例化User对象
+        $count =$coding->where($where)->count();// 查询满足要求的总记录数
+		
+        $Page = new \Think\Page($count,15,$parameter);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$show = $Page->show();// 分页显示输出
+
+    	$list=D("coding")->field("cd.*,gs.name")->alias("cd")->join("__GOODS__ gs on gs.coding=cd.code",'left')->order("cd.createtime","desc")->where($where)->limit($Page->firstRow.','.$Page->listRows)->select();
   
     	$this->assign('list',$list);
+    	$this->assign('page',$show);// 赋值分页输出
     	$this->display();
     }
 
