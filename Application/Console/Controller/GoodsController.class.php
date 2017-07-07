225f08786e61bdec791b6db803d1ecff2e9d662f
zharty <zharty@163.com>
2017-07-07 14:35:19 +0800

    数据

--- Application/Console/Controller/GoodsController.class.php
+++ Application/Console/Controller/GoodsController.class.php
@@ -50,7 +50,7 @@ class GoodsController extends Controller {
     }
     //快递记录表
     public function purche(){
-   		if(session("userid")!=6){
+   		if(session("userid")!=6&&session("userid")!=37&&session("userid")!=190){
    			$where['cd.userid']=session("userid");
    		}
 
